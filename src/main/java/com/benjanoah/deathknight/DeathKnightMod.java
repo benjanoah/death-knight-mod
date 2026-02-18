81f516d08cc6af2fb27e0134dcb56f4985aee64f
Ruben Neuteboom <ruben.neuteboom@gmail.com>
2026-02-18 17:26:24 +0100

    Fix: BiomeModifications toevoegen voor Soul Sand Valley structure

--- src/main/java/com/benjanoah/deathknight/DeathKnightMod.java
+++ src/main/java/com/benjanoah/deathknight/DeathKnightMod.java
@@ -4,6 +4,12 @@ import com.benjanoah.deathknight.entity.ModEntities;
 import com.benjanoah.deathknight.item.ModItems;
 import com.benjanoah.deathknight.world.structure.ModStructures;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.BiomeKeys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +23,13 @@ public class DeathKnightMod implements ModInitializer {
         ModStructures.register();
         ModEntities.register();
         ModItems.register();
+
+        // Voeg het kasteel toe aan de Soul Sand Valley biome
+        BiomeModifications.addStructure(
+            BiomeSelectors.includeByKey(BiomeKeys.SOUL_SAND_VALLEY),
+            RegistryKey.of(RegistryKeys.STRUCTURE_SET, new Identifier(MOD_ID, "death_knight_castle"))
+        );
+
         LOGGER.info("Death Knight Mod ready! 💀⚔️");
     }
 }
