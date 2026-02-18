21e28bc2992bfecc7507e971a8c33814da13eed4
Ruben Neuteboom <ruben.neuteboom@gmail.com>
2026-02-18 09:06:07 +0100

    Fix: spawn egg toevoegen aan creative tab

--- src/main/java/com/benjanoah/deathknight/item/ModItems.java
+++ src/main/java/com/benjanoah/deathknight/item/ModItems.java
@@ -3,7 +3,9 @@ package com.benjanoah.deathknight.item;
 import com.benjanoah.deathknight.DeathKnightMod;
 import com.benjanoah.deathknight.entity.ModEntities;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -26,6 +28,11 @@ public class ModItems {
     );
 
     public static void register() {
+        // Voeg spawn egg toe aan de Spawn Eggs tab in creative
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(entries -> {
+            entries.add(DEATH_KNIGHT_SPAWN_EGG);
+        });
+
         DeathKnightMod.LOGGER.info("Death Knight items registered!");
     }
 }
